6d4d18feff5f773e5cbc52eb72eb898fc18b58d6
Peter Benis <benispeti@gmail.com>
2017-05-24 20:03:58 +0200

    Update start script

--- start_cornerstone.sh
+++ start_cornerstone.sh
@@ -6,9 +6,6 @@
 sudo docker build -t=$CORNERSTONE_IMAGE github.com/digitallyseamless/docker-nodejs-bower-grunt
 
 sudo docker run --name $CORNERSTONE_CONTAINER --rm \
-    --hostname $CORNERSTONE_HOSTNAME \
-    --publish $CORNERSTONE_PORT:$CORNERSTONE_PORT \
-    --restart unless-stopped \
     -v $(pwd):/app -w /app \
     $CORNERSTONE_IMAGE \
     sh -c "npm install; bower install; grunt"
